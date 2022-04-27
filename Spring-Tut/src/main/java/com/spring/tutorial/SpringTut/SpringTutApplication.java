50e1c524dd4289f629c7fa8957f21144737b40d3
p-artie <artiepolanco@gmail.com>
2022-04-27 17:38:00 -0400

    Initial Spring project commit

---
+++ Spring-Tut/src/main/java/com/spring/tutorial/SpringTut/SpringTutApplication.java
@@ -0,0 +1,13 @@
+package com.spring.tutorial.SpringTut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutApplication.class, args);
+	}
+
+}
---
+++ Spring-Tut/src/test/java/com/spring/tutorial/SpringTut/SpringTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tutorial.SpringTut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
